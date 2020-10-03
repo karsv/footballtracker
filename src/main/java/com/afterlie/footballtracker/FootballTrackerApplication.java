b57a2107cd580e3d1ec6419502bb0f19530bd5ef
karsv <Pbvf2019!>
2020-10-03 16:31:10 +0300

    Initial commit

---
+++ src/main/java/com/afterlie/footballtracker/FootballTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.afterlie.footballtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/afterlie/footballtracker/FootballTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afterlie.footballtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
