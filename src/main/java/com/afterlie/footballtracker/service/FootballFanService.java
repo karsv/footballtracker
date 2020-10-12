40481f906350709c5b26cf99e5332909efa7b13b
karsv <Pbvf2019!>
2020-10-12 11:22:03 +0300

    add footbalFan service

---
+++ src/main/java/com/afterlie/footballtracker/service/FootballFanService.java
@@ -0,0 +1,20 @@
+package com.afterlie.footballtracker.service;
+
+import com.afterlie.footballtracker.dto.FootballFanDto;
+import com.afterlie.footballtracker.dto.FootballFanRegistrationDto;
+import com.afterlie.footballtracker.model.FootballFan;
+import java.util.List;
+
+public interface FootballFanService {
+    FootballFanDto create(FootballFanRegistrationDto footballFanRegistrationDto);
+
+    FootballFanDto update(FootballFan footballFan);
+
+    FootballFanDto getFootballFanById(Long id);
+
+    FootballFanDto getFootballFanByName(String name);
+
+    List<FootballFanDto> getAllFootballFans();
+
+    boolean deleteFootballFanById(Long id);
+}
