1197c2a694d152d944aa0df1946508c6a73416b0
karsv <Pbvf2019!>
2020-10-14 20:32:51 +0300

    add constraints for FootballFanRegistrationDto

--- src/main/java/com/afterlie/footballtracker/dto/FootballFanRegistrationDto.java
+++ src/main/java/com/afterlie/footballtracker/dto/FootballFanRegistrationDto.java
@@ -1,8 +1,19 @@
 package com.afterlie.footballtracker.dto;
 
+import com.afterlie.footballtracker.passwordEqualValidator.PasswordEqualValidator;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@PasswordEqualValidator
 public class FootballFanRegistrationDto {
+    @NotNull(message = "Football fan must have name!")
+    @Size(min = 3, message = "Football fans name must get more then 3 symbols!")
     private String name;
+    @NotNull(message = "FF must have password!")
+    @Size(min = 3, message = "password must be more then 3 symbols!")
     private String password;
+    @NotNull(message = "you must repeat password!")
+    @Size(min = 3, message = "Repeated password must be the same then password!")
     private String repeatPassword;
 
     public String getName() {
