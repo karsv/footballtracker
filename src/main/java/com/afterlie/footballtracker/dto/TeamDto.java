939eb9ea343a19abffe7997d37de4e0368f75f65
karsv <Pbvf2019!>
2020-10-23 15:41:29 +0300

    fix team dto

--- src/main/java/com/afterlie/footballtracker/dto/TeamDto.java
+++ src/main/java/com/afterlie/footballtracker/dto/TeamDto.java
@@ -1,10 +1,21 @@
 package com.afterlie.footballtracker.dto;
 
+import java.util.Objects;
+
 public class TeamDto {
+    private Long id;
     private String teamName;
     private String country;
 
     public TeamDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTeamName() {
@@ -21,5 +32,20 @@ public class TeamDto {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TeamDto teamDto = (TeamDto) o;
+        return Objects.equals(id, teamDto.id) &&
+                Objects.equals(teamName, teamDto.teamName) &&
+                Objects.equals(country, teamDto.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, teamName, country);
     }
 }
